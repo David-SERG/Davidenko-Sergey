0fdb15846c04d17623f754f32062629a9e09a24c
Davidenko-Sergey <davidsergey@ukr.net>
2018-01-28 12:24:27 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	CaheeNew/index.html

---
+++ CssAnimationHome/js/main.js
@@ -0,0 +1,10 @@
+;(function ($) {
+    $(function () {
+
+    $(".cards-container").on('click', function () {
+        console.log(567);
+        $(this).toggleClass("value3");
+
+    });
+    })
+}) (jQuery);
